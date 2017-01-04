211fe584ba5f5fda8f0b7cc86566d05db504cd96
Ceyhun Coban <ceyhun.coban@gmail.com>
2017-01-04 04:07:43 +0000

    First commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
@@ -9,6 +12,25 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+  ticker = request.form['text']
+  pticker = ticker.upper()
+  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % pticker
+  session = requests.Session()
+  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+  raw_data = session.get(api_url)
+  
+  plot = figure(tools=TOOLS,
+              title='Data from Quandle WIKI set',
+              x_axis_label='date',
+              x_axis_type='datetime')
+
+  script,div = components(plot)
+  return render_template('graph.html', script=script, div=div)
+  
+
 
 if __name__ == '__main__':
   app.run(port=33507)
